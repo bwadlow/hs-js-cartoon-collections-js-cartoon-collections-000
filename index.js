39d945485deaf90000f54505f8ff6307786218ad
Brian Wadlow <bwadlow@gmail.com>
2018-06-16 22:11:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function dwarfRollCall(dwarves) {
+  return dwarves.map((elem, i) => i+1+'. '  + elem).join(' ')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(elem => elem.toUpperCase()+'!')
 }
 
 function longPlaneteerCalls(words) {
+  let newArr=words.filter(elem => elem.length>4)
+  return  !(newArr.length) ? false : true
 }
 
 function findTheCheese (foods) {
+  let cheese=['cheddar','gouda','camembert']
+  let newArr=foods.filter(elem => cheese.indexOf(elem) > -1)
+  return !(newArr.length) ? 'no cheese!' : newArr[0]
 }
